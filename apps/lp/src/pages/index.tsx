a0ec36dfe5c3595d0a0fa52be9bf54d7aaaeb162
alaodriss <alaodriss@gmail.com>
2023-03-30 15:16:14 +0200

    improve hero section

--- apps/lp/src/pages/index.tsx
+++ apps/lp/src/pages/index.tsx
@@ -4,35 +4,70 @@ import tw from "tailwind-styled-components"
 import Layout from "../components/layout/index"
 import { Seo } from "../components/layout"
 
-const Title = tw.span`text-5xl text-green-500 uppercase`
+const Title = tw.span`text-5xl text-green-500 uppercase font-bold`
 const Caption = tw.span`text-3xl text-gray-900 uppercase`
 
-const Description = tw.h1`text-2xl text-gray-500`
+const Description = tw.h1`text-2xl text-gray-500 mt-6`
 const Separator = tw.div`w-10 h-1 bg-blue-500`
 const MainImage = tw.img`rounded-3xl shadow-lg`
 const Setcion = tw.div`max-w-screen-2xl mx-auto relative`
-const Button = tw.button`bg-green-500 p-5 py-3 rounded-xl text-white font-bold uppercase`
+const Button = tw.button`bg-green-500 p-5 py-3 rounded-xl text-white font-bold uppercase mt-6`
 const Gradient = tw.div`absolute inset-0 bg-gradient-radial-hero`
 
-const LogoTitle = () => (
+const heroFeatures = [
+  {
+    title: "Book Easy",
+    description: " Tickets with QR Code",
+  },
+  {
+    title: "Discounts",
+    description: " For those who create an account",
+  },
+  {
+    title: "30+",
+    description: "Cities around the US",
+  },
+]
+
+const HeroFeaturesItem = ({ item }) => (
   <div>
+    <h5 className="font-bold" >{item.title}</h5>
+    <p>{item.description}</p>
+  </div>
+)
+
+const HeroFeatures = () => (
+  <div className="flex gap-4">
+    {heroFeatures.map(item => (
+      <HeroFeaturesItem item={item} />
+    ))}
+  </div>
+)
+
+const LogoTitle = () => (
+  <div className="mt-6">
     <Caption>{"The private"}</Caption>
     <div className="mt-1">
       <Title className="text-blue-500">{"Bus"}</Title>
       <Title className="ml-3 text-green-500">{"Company"}</Title>
     </div>
-    <Separator className="mt-4"/>
+    <Separator className="mt-4" />
   </div>
 )
 const TextSection = () => (
-  <div className="space-y-6">
-    <LogoTitle />
-    <Description>
-      {
-        "This is a private bus company that takes you from point  B within the Continental US."
-      }
-    </Description>
-    <Button>{"Create Account"}</Button>
+  <div className="mt-6">
+    <div className="space-y-6 ">
+      <LogoTitle />
+      <Description>
+        {
+          "This is a private bus company that takes you from point  B within the Continental US."
+        }
+      </Description>
+      <Button>{"Create Account"}</Button>
+    </div>
+    <div className="mt-32">
+      <HeroFeatures />
+    </div>
   </div>
 )
 
