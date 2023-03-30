7fd32eb5ac7e1bcd88ff812a11bc1b460a006805
alaodriss <alaodriss@gmail.com>
2023-03-30 11:14:08 +0200

    install gatsby

---
+++ apps/lp/src/pages/index.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
+
+
+
+const hedingString: string = "hello all peopel"
+const IndexPage = () => (
+  <Layout>
+     <h1>{hedingString}</h1>
+  </Layout>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="Home" />
+
+export default IndexPage
